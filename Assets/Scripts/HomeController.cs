30083c60fff889f93898b9326b5518344158e732
Денис Братусев <55459712+bratusev07@users.noreply.github.com>
2024-08-20 14:59:54 +0300

    Best score show in main HomeScreen with prefernces

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class HomeController : MonoBehaviour
 {
 
+    public void Start()
+    {
+        GameObject.Find("BestScoreText").GetComponent<TextMeshProUGUI>().text = "Best score: " + PlayerPrefs.GetInt("BestScore");
+    }
+
     public void ChangeScene()
     {
         SceneManager.LoadSceneAsync(0);
         if (PlayerPrefs.GetInt("BestScore") < ScoreColliderScript.score) PlayerPrefs.SetInt("BestScore", ScoreColliderScript.score);
-        Debug.Log(PlayerPrefs.GetInt("BestScore"));
         ScoreColliderScript.score = 0;
     }
 }
